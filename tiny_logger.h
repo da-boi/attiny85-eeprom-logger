1708d4c8f0b4eb4abbc13bfb3eaf999e53ff686c
Unknown <nick@michaelis-hamburg.eu>
2018-12-31 19:49:05 +0100

    Added function description to header

--- tiny_logger.h
+++ tiny_logger.h
@@ -5,7 +5,18 @@
 	#define LOG_OFFSET 2
 	#define LOG_P (void *)0
 
+	/*
+	 * Sets the log pointer to zero
+	 * first byte is now a null character
+	 * Data not actually overwritten - can still be recovered with a hex dump
+	 * 		- when not overriden by log_push()
+	*/
 	void log_clear(void);
+
+	/*
+	 * Appends the given string to the log
+	 * Terminates the log with a null character
+	*/
 	int log_push(const char *string);
 
 #endif
